2c1d2ec7a18e93bce5f45dae70738ab7a73c83de
SamIlias <Samovichilias19life@gmail.com>
2025-10-07 22:20:17 +0300

    fix: add needed dependencies for eslint

--- src/stories/DatePicker.stories.tsx
+++ src/stories/DatePicker.stories.tsx
@@ -1,4 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react-webpack5';
+
 import { DatePicker } from '../components/DatePicker';
 
 const meta: Meta<typeof DatePicker> = {
